655d76be01a6eb68b8048ef1d28151a9e1ea6298
radhakrishna20 <radhakrishna6k@gmail.com>
2017-02-19 20:37:07 -0600

    Initial commit

---
+++ application.js
@@ -0,0 +1,23 @@
+var Calculator = {
+
+  initialize : function() {
+    $("button, input:button, a", "#calculator").button();
+
+    $('#calculator .numbers input:not(#sum)').click( function() {
+        var original_result = $('#result').attr('value');
+        $('#result').attr( 'value', original_result + this.value )
+    } );
+
+    $('#calculator .operators input').click( function() {
+        var original_result = $('#result').attr('value');
+        $('#result').attr( 'value', eval(original_result) + this.value )
+    } );
+
+    $('#calculator input#sum').click( function() {
+      $('#result').attr( 'value', eval( $('#result').attr('value') ) )
+    } );
+  }
+
+};
+
+jQuery(document).ready( function() { Calculator.initialize() } );
